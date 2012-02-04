612defdf7a5abfd47de1b6c8ed70c8f7e28e3817
Michael <michael@nodeone.se>
2012-02-04 12:58:18 +0100

    Udpate music player scripts and tpl files and add fittext script and add css to music player

--- profiles/socialanimal/themes/socialanimal/scripts/sa.js
+++ profiles/socialanimal/themes/socialanimal/scripts/sa.js
@@ -1,13 +1,21 @@
 (function ($) {
 
-
-    Drupal.behaviors.brsvFaqToggle = {
+// Music player hide/display function
+    Drupal.behaviors.SaMusicToggle = {
     attach: function (context, settings) {
-      $("#music-player .views-field-title", context).click(function(e) {
-        $('.jp-interface').toggle('fast');
-        $('.jp-playlist').toggle('fast');
-       // $('.jp-audio').addClass('music-player-hide');
+      $("#music-player .player-hide-show", context).click(function(e) {
+       // $('.jp-interface').toggle('fast');
+        $('.jp-playlist, .views-field-field-music-image, .views-field-title').toggle('fast');
+       // $('.views-field-field-music-image').toggle('fast');
+       // $('.views-field-title').toggle('fast');
       })
+    }
+  }
+
+// Scale text function
+  Drupal.behaviors.saFitText = {
+    attach: function (context, settings) {
+      $(".pane-page-site-name .pane-content").fitText((0.8), { minFontSize: '50px', maxFontSize: '130px' });
     }
   }
 
